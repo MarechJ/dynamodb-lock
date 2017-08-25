c7421084262e2ecd1a40820287e90885fbdb9d07
Julien Marechal <julien.marechal35@gmail.com>
2017-08-25 15:58:52 +0200

    patch version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='julien.marechal35@gmail.com',
     license='MIT',
     url='https://github.com/MarechJ/dynamodb-lock',
-    download_url='https://github.com/MarechJ/dynamodb-lock/archive/0.1.tar.gz',
+    download_url='https://github.com/MarechJ/dynamodb-lock/archive/0.1.1.tar.gz',
     keywords=['dynamo', 'dynamodb', 'aws', 'lock', 'distributed', 'locking'],
     install_requires=[
         "boto3>=1.4.5"
